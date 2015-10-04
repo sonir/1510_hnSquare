4347c6d73b7c88beb0c30dd0878bf30e8cbe35e2
sonir <isana@sonir.jp>
2015-10-04 23:32:28 +0900

    Ver0 start convertion

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# 1510_hnSquare" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sonir/1510_hnSquare.git
+git push -u origin master
